c78a7a4fbe4c066b7fefbf5d96bac9b706dd4e8a
ArthurMcM1727 <zachmcmillanco@outlook.com>
2026-01-29 12:15:08 -0700

    learned variable assignment, basic functions, prompts, console.log and alerts

--- HTML/week_four/script.js
+++ HTML/week_four/script.js
@@ -1,5 +1,30 @@
-alert("Welcome to WSU")
-alert('Enjoy')
+alert("Course registration")
 
 let num1 = 5
-let num2 = 4.6
+student ={
+    str: "Hello World",
+    age: 30,
+    //write a function to ask user what is your name
+    // store the name in the variable str
+    //use console to print welcom statement for that student
+    
+    F_welcome(){
+        let str=prompt("What is your name: ", [])
+        if(str != "" & str!= "blah")
+            console.log("Welcome " + str + " to WSU")
+        else{
+            console.log("Please provide a name")
+            student.F_welcome()
+        }
+    }
+}
+
+
+student.F_welcome()
+
+
+
+let str = "Hello World"
+
+let age = 30, flag=true;
+
