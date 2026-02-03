ed60ba2ae8fe1bd3bf04148178d1d2300ecebcb3
ArthurMcM1727 <zachmcmillanco@outlook.com>
2026-02-03 12:02:11 -0700

    working on ternerary comparitors, and simple conversions

--- HTML/week_five/script.js
+++ HTML/week_five/script.js
@@ -3,9 +3,65 @@
 // let result = confirm("Do you like Javascript?")
 // console.log("Do you like JS? The answer is: " + result)
 
-let var1 = 3.5
-let str = "Hello World", age = 23
-console.log(str.length)
+// let var1 = 3.5
+// let str = "Hello World", age = 23 //primative variables
+// console.log(str.length)
 
-console.log(typeof Number(str))
-console.log(Number(str))
+// let person = {
+//     p_name: "John",
+//     p_hieght: 5.6,
+//     sayHi(){
+//         console.log(`Hello ${person.p_name}`)
+//     },
+//     p_print(){
+//         console.log(person)
+//     }
+// }
+
+// person.sayHi()
+// person.p_print()
+
+// let var1 = 3.5
+// let str = "123", age = 23
+// console.log(str.length)
+
+// console.log(typeof Number(str))
+// console.log(Number(str))
+
+// // undefined becomes NaN as a Number
+// // null becomes 0 as a Number
+// // true/false becomes 1/0  as a Number
+// // string becomes NaN as a Number
+
+// let result = "5" - 2 // equals 3
+// let result1 = "5" + 2 // equals 52 because it performed concatination instead of math
+// let result2 = "5" * 2 // equals 10
+
+// 5 > 3 //true
+// 5 < 3 //false
+// 5 >= 5 //true
+// 5 == 5 //true
+// 5 == "5" // true
+
+// 5 === "5" // false (no type conversion) === performs strict comparison
+// 5 == '5' // true (performes type conversion)
+
+// let x = null
+// let result = x ?? "Hello"
+// console.log(result) // prints: Hello
+
+// let age = 20
+// let access = (age >= 10) ? "Allowed" : "Denied"
+// console.log(access) //prints: Allowed
+
+// let str = "123"
+// let num = +str // converts into an integer
+
+// let r = 5 + 2 / 2 * 3 //prints 8 (muliplication gets priority)
+
+let n1  = prompt("enter first number")
+let n2 = prompt("enter second number")
+
+console.log(n1 + "     " + n2)
+let result = (n1 > n2) ? `${n1} is greater` : `${n2} is greater`
+console.log(result)
