5f6df8381f35390ae77a07a8f95fd78cb10cb7a6
ArthurMcM1727 <zachmcmillanco@outlook.com>
2026-02-03 11:35:27 -0700

    began working with the logic of javascript and understanding some basic rules

---
+++ HTML/week_five/script.js
@@ -0,0 +1,11 @@
+// alert("Welcome to this page")
+// prompt("What is your Fav language", "C++")
+// let result = confirm("Do you like Javascript?")
+// console.log("Do you like JS? The answer is: " + result)
+
+let var1 = 3.5
+let str = "Hello World", age = 23
+console.log(str.length)
+
+console.log(typeof Number(str))
+console.log(Number(str))
